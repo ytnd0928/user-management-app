9e57c388d6980518c77848b2f6cd276f603b884e
nodachan <72251931+ytnd0928@users.noreply.github.com>
2021-06-16 22:21:50 +0900

    create-login-template create-login-template

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,29 @@
 import { memo, VFC } from "react";
+import {
+  Flex,
+  Box,
+  Heading,
+  Divider,
+  Input,
+  Button,
+  Stack
+} from "@chakra-ui/react";
 
 export const Login: VFC = memo(() => {
-  return <p>ログインページです</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <Button bg="teal.400" color="hwite" _hover={{ opacity: 0.8 }}>
+            ログイン
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
