05d0d691e96d17ec7d5cd7a0b1d086cdd5dbef06
nodachan <72251931+ytnd0928@users.noreply.github.com>
2021-06-26 11:11:15 +0900

    userCard UserCard

---
+++ src/components/organism/user/UserCard.tsx
@@ -0,0 +1,39 @@
+import { memo, VFC } from "react";
+import { Box, Stack, Image, Text } from "@chakra-ui/react";
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+
+export const UserCard: VFC<Props> = memo((props) => {
+  const { imageUrl, userName, fullName } = props;
+  return (
+    <Box
+      p={4}
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+      w="260px"
+      h="260px"
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+    >
+      <Stack textAlign="center">
+        <Image
+          boxSize="160px"
+          margin="auto"
+          borderRadius="full"
+          src={imageUrl}
+          alt={userName}
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          noda
+        </Text>
+        <Text fontSize="small" fontWeight="bold" color="gray">
+          NODACHAN
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,36 +1,17 @@
 import { memo, VFC } from "react";
 import { Text, Image, Box, Stack, Wrap, WrapItem } from "@chakra-ui/react";
+import { UserCard } from "../organism/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   //divタグの代わりとなるBoxを使用
   return (
     <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box
-          p={4}
-          _hover={{ cursor: "pointer", opacity: 0.8 }}
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-        >
-          <Stack textAlign="center">
-            <Image
-              boxSize="160px"
-              alt="プロフィール画像"
-              margin="auto"
-              borderRadius="full"
-              src="https://source.unsplash.com/randam"
-            />
-            <Text fontSize="lg" fontWeight="bold">
-              noda
-            </Text>
-            <Text fontSize="small" fontWeight="bold" color="gray">
-              NODACHAN
-            </Text>
-          </Stack>
-        </Box>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="のだちゃん"
+          fullName="nodachan"
+        />
       </WrapItem>
     </Wrap>
   );
