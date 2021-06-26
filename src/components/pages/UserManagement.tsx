3190de2470e729c53af0eedf6485912d7659dde6
nodachan <72251931+ytnd0928@users.noreply.github.com>
2021-06-26 10:54:29 +0900

    create user card create user card

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,16 +1,37 @@
 import { memo, VFC } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { Text, Image, Box, Stack, Wrap, WrapItem } from "@chakra-ui/react";
 
 export const UserManagement: VFC = memo(() => {
+  //divタグの代わりとなるBoxを使用
   return (
-    <Wrap>
-      {[...Array(10)].map(() => (
-        <WrapItem>
-          <div
-            style={{ width: "100px", height: "100px", backgroundColor: "teal" }}
-          />
-        </WrapItem>
-      ))}
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box
+          p={4}
+          _hover={{ cursor: "pointer", opacity: 0.8 }}
+          w="260px"
+          h="260px"
+          bg="white"
+          borderRadius="10px"
+          shadow="md"
+        >
+          <Stack textAlign="center">
+            <Image
+              boxSize="160px"
+              alt="プロフィール画像"
+              margin="auto"
+              borderRadius="full"
+              src="https://source.unsplash.com/randam"
+            />
+            <Text fontSize="lg" fontWeight="bold">
+              noda
+            </Text>
+            <Text fontSize="small" fontWeight="bold" color="gray">
+              NODACHAN
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
     </Wrap>
   );
 });
